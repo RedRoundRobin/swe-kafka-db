41d0fea01bf9ed5c1aef0e7a20138539bc3dbcd5
Alessandro <tommasinalessandro@gmail.com>
2020-03-11 18:25:46 +0100

    Creato timescale db

---
+++ database/Timescale/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE conditions (
+  time        TIMESTAMPTZ       NOT NULL,
+  location    TEXT              NOT NULL,
+  temperature DOUBLE PRECISION  NULL,
+  humidity    DOUBLE PRECISION  NULL
+);
+
+SELECT create_hypertable('conditions', 'time');
