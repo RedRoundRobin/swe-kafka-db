709e25c0d74de28b031244d876bd32867b514569
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-04-23 18:26:52 +0200

    Test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM adoptopenjdk/maven-openjdk11:latest
-# COPY . /usr/src/data-collector/tmp
-# WORKDIR /usr/src/data-collector/tmp
-WORKDIR .
-ENTRYPOINT ["sh", "start.sh"]
+COPY start.sh /usr/local/bin
+RUN ln -s /usr/local/bin/start.sh /
+ENTRYPOINT ["start.sh"]
--- start.sh
+++ start.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
 
-IFS='
-'
+if [ ! -f ../../../kafka-data-collector.jar ]; then
 
-if [ ! -f ./kafka-data-collector.jar ]; then
-
-	cd kafka-data-collector
+	cd ../../../kafka-data-collector
 
 	mvn clean package
 
-	mv -f target/kafka-data-collector*dependencies.jar ../kafka-data-collector.jar
-	cd ../..
-	rm -rf kafka-data-collector
-	rm -rf .github
-	rm -rf kafka
-	rm -f .gitignore
-	rm -f LICENSE
-	rm -f README.md
+	mv -f target/kafka-data-collector*dependencies.jar ../../../../kafka-data-collector.jar
+	cd ../../../
+	rm -rf ../../../kafka-data-collector
+	rm -rf ../../../.github
+	rm -rf ../../../kafka
+	rm -f ../../../.gitignore
+	rm -f ../../../LICENSE
+	rm -f ../../../README.md
 	
 fi
 
-java -jar ./kafka-data-collector.jar
+java -jar ../../../kafka-data-collector.jar
 
 echo "Kafka data collector started..."
