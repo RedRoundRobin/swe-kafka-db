b99c21839ec3d94916674de2df8323e1bbb4ff61
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-04-23 18:00:55 +0200

    Test start-stop container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM adoptopenjdk/maven-openjdk11:latest
-COPY . /usr/src/data-collector/tmp
-WORKDIR /usr/src/data-collector/tmp
+# COPY . /usr/src/data-collector/tmp
+# WORKDIR /usr/src/data-collector/tmp
+WORKDIR .
 CMD ["sh", "start.sh"]
--- start.sh
+++ start.sh
@@ -3,18 +3,23 @@
 IFS='
 '
 
-if [ ! -f /usr/src/data-collector/kafka-data-collector.jar ]; then
+if [ ! -f ./kafka-data-collector.jar ]; then
 
 	cd kafka-data-collector
 
 	mvn clean package
 
-	mv -f target/kafka-data-collector*dependencies.jar ../../kafka-data-collector.jar
+	mv -f target/kafka-data-collector*dependencies.jar ../kafka-data-collector.jar
 	cd ../..
-	rm -rf tmp 
+	rm -rf kafka-data-collector
+	rm -rf .github
+	rm -rf kafka
+	rm -f .gitignore
+	rm -f LICENSE
+	rm -f README.md
 	
 fi
 
-java -jar /usr/src/data-collector/kafka-data-collector.jar
+java -jar ./kafka-data-collector.jar
 
 echo "Kafka data collector started..."
